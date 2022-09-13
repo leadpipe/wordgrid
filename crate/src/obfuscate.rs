707b48f09c9d0c4712186fd1c1775ac8244e9763
Luke Blanshard <luke@blanshard.us>
2022-09-12 20:38:49 -0500

    Adds a check byte to the obfuscated string.

--- crate/src/obfuscate.rs
+++ crate/src/obfuscate.rs
@@ -1,13 +1,23 @@
-use base64::{encode_config, URL_SAFE_NO_PAD, decode_config};
+use std::num::Wrapping;
+
+use base64::{decode_config, encode_config, URL_SAFE_NO_PAD};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 use crate::JsRandom;
 
 /// Obfuscates a slice of bytes by xor-ing them with random bytes from the given
-/// generator, then encodes them using URL-safe base64.
+/// generator, appends a check byte, then encodes them using URL-safe base64.
 #[wasm_bindgen]
 pub fn obfuscate(bytes: &[u8], rng: &mut JsRandom) -> String {
-  let xored_bytes: Vec<u8> = bytes.iter().map(|&b| rng.next_byte() ^ b).collect();
+  let mut sum = Wrapping(0u8);
+  let mut xored_bytes: Vec<u8> = bytes
+    .iter()
+    .map(|&b| {
+      sum += b;
+      rng.next_byte() ^ b
+    })
+    .collect();
+  xored_bytes.push(sum.0 ^ rng.next_byte());
   encode_config(xored_bytes, URL_SAFE_NO_PAD)
 }
 
@@ -15,12 +25,23 @@ pub fn obfuscate(bytes: &[u8], rng: &mut JsRandom) -> String {
 /// string and then xor-ing the resulting bytes with bytes from the given
 /// generator (which must have the same initial state as the one passed to
 /// `obfuscate`).
-/// 
+///
 /// Panics on input that can't be decoded.
 #[wasm_bindgen]
 pub fn deobsuscate(input: String, rng: &mut JsRandom) -> Vec<u8> {
-  let xored_bytes = decode_config(input, URL_SAFE_NO_PAD).unwrap();
-  xored_bytes.iter().map(|&b| rng.next_byte() ^ b).collect()
+  let mut sum = Wrapping(0u8);
+  let mut xored_bytes = decode_config(input, URL_SAFE_NO_PAD).unwrap();
+  let check = xored_bytes.pop().unwrap();
+  let answer = xored_bytes
+    .iter()
+    .map(|&b| {
+      let orig = rng.next_byte() ^ b;
+      sum += orig;
+      orig
+    })
+    .collect();
+  assert!(check == sum.0 ^ rng.next_byte(), "Check byte mismatch");
+  answer
 }
 
 #[cfg(test)]
@@ -29,19 +50,19 @@ mod tests {
 
   #[test]
   fn test_round_trip() {
-    let input = vec![1, 2, 3, 4, 5];
+    let input = vec![1, 2, 3, 4];
     let string = obfuscate(input.as_slice(), &mut JsRandom::new("seed"));
-    assert_eq!(string.len(), 7);  // ceil(5 * 8 / 6)
+    assert_eq!(string.len(), 7); // ceil((4 + 1) * 8 / 6)
     let output = deobsuscate(string, &mut JsRandom::new("seed"));
     assert_eq!(input, output);
   }
 
   #[test]
+  #[should_panic(expected = "Check byte mismatch")]
   fn test_failed_round_trip() {
     let input = vec![1, 2, 3, 4, 5];
     let string = obfuscate(input.as_slice(), &mut JsRandom::new("seed"));
-    let output = deobsuscate(string, &mut JsRandom::new("different seed"));
-    assert_ne!(input, output);
+    deobsuscate(string, &mut JsRandom::new("different seed"));
   }
 
   #[test]
