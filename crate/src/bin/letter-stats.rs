2e183aac95ed38783bf668f8bba76d9f55d5ff3b
Luke Blanshard <luke@blanshard.us>
2022-09-25 16:12:39 -0500

    Sorts the letter stats by frequency.

--- crate/src/bin/letter-stats.rs
+++ crate/src/bin/letter-stats.rs
@@ -13,16 +13,21 @@ fn main() {
   let start = Instant::now();
   let builder = fill_builder_from_file("../words-v1.txt");
   let duration = start.elapsed();
-  let total = builder.total_letter_count();
+  let total = builder.total_letter_count() as f64;
   println!(
     "{} letters loaded in {:?}",
     total.separate_with_commas(),
     duration
   );
   println!();
-  for letter in builder.letters() {
-    let count = builder.letter_count(&letter);
-    let percentage = 100f64 * (count as f64) / (total as f64);
+  let mut counts: Vec<(char, f64)> = builder
+    .letters()
+    .iter()
+    .map(|letter| (*letter, builder.letter_count(letter) as f64))
+    .collect();
+  counts.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+  for (letter, count) in counts {
+    let percentage = 100f64 * count / total;
     println!(
       "{}: {:>8} ({:.2}%)",
       letter,
