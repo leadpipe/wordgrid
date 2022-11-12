7757f0ac76c7628dd3575688c6b0485075762ea9
Luke Blanshard <luke@blanshard.us>
2022-11-12 15:53:07 -0600

    Updates the grid-word-stats program with histograms

--- crate/src/bin/grid-word-stats.rs
+++ crate/src/bin/grid-word-stats.rs
@@ -6,23 +6,27 @@ use std::{
 };
 
 use chrono::{Duration, Local, NaiveDate};
+use histogram::*;
 use leadpipe_wordgrid::*;
+use thousands::Separable;
 
-type CountDate = (usize, NaiveDate);
-type CountRange = (CountDate, CountDate);
 struct PuzzleSize {
+  name: &'static str,
   size: usize,
   min_length: usize,
 }
 const SMALL: PuzzleSize = PuzzleSize {
+  name: "Small",
   size: 4,
   min_length: 3,
 };
 const MEDIUM: PuzzleSize = PuzzleSize {
+  name: "Medium",
   size: 5,
   min_length: 4,
 };
 const LARGE: PuzzleSize = PuzzleSize {
+  name: "Large",
   size: 6,
   min_length: 5,
 };
@@ -43,32 +47,21 @@ fn main() {
   let duration = load.elapsed();
   println!("{} words loaded in {:?}", words.count(), duration);
 
-  let mut date = start;
-  let small = count_and_date(&words, date, SMALL);
-  let medium = count_and_date(&words, date, MEDIUM);
-  let large = count_and_date(&words, date, LARGE);
-  let mut small_range = (small, small);
-  let mut medium_range = (medium, medium);
-  let mut large_range = (large, large);
-  while date < end {
-    date += Duration::days(1);
-    small_range = update_range(small_range, &words, date, SMALL);
-    medium_range = update_range(medium_range, &words, date, MEDIUM);
-    large_range = update_range(large_range, &words, date, LARGE);
-  }
+  let generate = Instant::now();
+  let small = build_word_count_histogram(&words, start, end, &SMALL);
+  let medium = build_word_count_histogram(&words, start, end, &MEDIUM);
+  let large = build_word_count_histogram(&words, start, end, &LARGE);
+  let duration = generate.elapsed();
+  println!(
+    "{} puzzles generated in {:?}",
+    (3 * small.0.entries()).separate_with_commas(),
+    duration
+  );
+  println!();
 
-  println!(
-    "Small:   min {} on {}; max {} on {}",
-    small_range.0 .0, small_range.0 .1, small_range.1 .0, small_range.1 .1
-  );
-  println!(
-    "Medium:  min {} on {}; max {} on {}",
-    medium_range.0 .0, medium_range.0 .1, medium_range.1 .0, medium_range.1 .1
-  );
-  println!(
-    "Large:   min {} on {}; max {} on {}",
-    large_range.0 .0, large_range.0 .1, large_range.1 .0, large_range.1 .1
-  );
+  print_histogram(small);
+  print_histogram(medium);
+  print_histogram(large);
 }
 
 fn build_words_from_file(fname: &str) -> Words {
@@ -81,23 +74,40 @@ fn build_words_from_file(fname: &str) -> Words {
   builder.build()
 }
 
-fn count_words(words: &Words, date: NaiveDate, puzzle_size: PuzzleSize) -> usize {
+fn build_word_count_histogram(
+  words: &Words,
+  start: NaiveDate,
+  end: NaiveDate,
+  puzzle_size: &'static PuzzleSize,
+) -> (Histogram, &'static PuzzleSize) {
+  let mut h = Histogram::new();
+  let mut date = start;
+  while date < end {
+    h.increment(count_words(words, date, puzzle_size)).unwrap();
+    date += Duration::days(1);
+  }
+  (h, puzzle_size)
+}
+
+fn count_words(words: &Words, date: NaiveDate, puzzle_size: &PuzzleSize) -> u64 {
   let seed = format!("1:{}:{}:1", date, puzzle_size.size);
   let mut random = new_random(&seed);
   let grid = Grid::new(words, puzzle_size.size, &mut random);
-  grid.find_words(words, puzzle_size.min_length).len()
+  grid.find_words(words, puzzle_size.min_length).len() as _
 }
 
-fn count_and_date(words: &Words, date: NaiveDate, puzzle_size: PuzzleSize) -> CountDate {
-  (count_words(words, date, puzzle_size), date)
-}
-
-fn update_range(
-  range: CountRange,
-  words: &Words,
-  date: NaiveDate,
-  puzzle_size: PuzzleSize,
-) -> CountRange {
-  let count = count_and_date(words, date, puzzle_size);
-  (std::cmp::min(count, range.0), std::cmp::max(count, range.1))
+fn print_histogram(h: (Histogram, &'static PuzzleSize)) {
+  println!(
+    "{} puzzles ({}) word counts by percentile:",
+    h.1.name,
+    h.0.entries().separate_with_commas()
+  );
+  for percentile in (0..=100).step_by(10) {
+    println!(
+      "{:>3}: {:>4}",
+      percentile,
+      h.0.percentile(percentile as f64).unwrap()
+    );
+  }
+  println!();
 }
